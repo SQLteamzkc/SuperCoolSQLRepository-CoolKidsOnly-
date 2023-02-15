415fb12a3b48824bada4be93cff388a789ebb603
KarterC86 <106718002+KarterC86@users.noreply.github.com>
2023-02-15 11:12:25 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -1,8 +1,9 @@
+
 def namesearch():
     input("Please input a Last Name or Player ID ")
     if str(input) == "LAST_NAME":
         print(createPlayer)
-    elif str(input) == "PHONE_NUMBER":
+    elif str(input) == "PHONE_NUMBER": #phone number is not a player ID
         print(createPlayer)
     else:
         input("Player Not Found, do you wish to create a new Player? Yes or No? ")
@@ -10,5 +11,7 @@ def namesearch():
             namesearch()
         else:
             print("Understood.")
+
+            #flip namesearch and the print(createPlayer)
             
 #NOTE: NOT COMPLETE BECAUSE I RAN INTO THE LAST_NAME ISSUE
