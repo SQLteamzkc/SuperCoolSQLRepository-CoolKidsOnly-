bc7222555b93fdab5c9078c408729adce5dd498c
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2023-02-15 13:06:37 -0500

    Part 3 Done

--- part3.py
+++ part3.py
@@ -1,4 +1,8 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv("creds.env")
 
 class MyServer:
 
@@ -8,7 +12,7 @@ class MyServer:
         self.connection = None
 
         try:
-            self.connection = mysql.connector.connect(host = '192.169.144.133', user = 'jrmcctc4', password = 'mcctcrocks', database = 'jr_team_4')
+            self.connection = mysql.connector.connect(host = os.getenv("HOST"), user = os.getenv("USER"), password = os.getenv("PASS"), database = os.getenv("DB"))
             print("Connection to server successful.")
 
         except mysql.connector.Error as e:
@@ -74,4 +78,37 @@ def namesearch():
         else:
             print("Understood.")
 
-namesearch()
+def deleteRecord():
+    #asks Player For ID
+    playerId = input("Input Player Id: ")
+
+    #Looks for Record with ID
+    mycursor.execute(
+        f"SELECT * FROM PLAYERS WHERE PLAYER_ID = {playerId}"
+    )
+
+    #Gets Results and Counts true Rows
+    results = mycursor.fetchall()
+    rowCount = mycursor.rowcount
+
+    if rowCount == 0:
+        print("Record Not Found.")
+    
+    else:
+        #Confirms a rsults been found and prints row
+        print(f"Record Found. {results}")
+
+        #Confirms Deletion request
+        deleteOption = input("Are you sure you would like to delete this record? Y/N: ")
+
+        if deleteOption == "Y" or deleteOption == "y":
+            mycursor.execute(
+                f"DELETE FROM PLAYERS WHERE PLAYER_ID = {playerId}"
+            )
+            mydb.connection.commit()
+        
+        else: 
+            #Part 4 is needed first
+            pass
+2
+deleteRecord()
