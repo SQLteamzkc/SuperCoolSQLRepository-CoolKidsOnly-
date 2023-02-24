91558b76785ce91f8b1fff127ac4613cf4f1c1c3
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2023-02-24 15:12:48 -0500

    Deleted sql server folder. Setting up flask

--- sqlwebsite/src/Components/Pages/Signup.js
+++ sqlwebsite/src/Components/Pages/Signup.js
@@ -2,13 +2,19 @@ import React, {useState} from 'react'
 import axios from 'axios'
 
 function Signup() {
-    const [emailReg, setEmailReg] = useState("")
-    const [passReg, setPassReg] = useState("")
+    const [firstNameReg, setFirstNameReg] = useState("")
+    const [lastNameReg, setLastNameReg] = useState("")
+    const [phoneNumberReg, setPhoneNumberReg] = useState("")
+    const [discordIDReg, setDiscordIDReg] = useState("")
+    const [esportsGameReg, setEsportsGameReg] = useState("")
 
     const register = () =>{ 
         axios.post('192.169.144.133:3306/PLAYERS', {
-            email: emailReg,
-            password: passReg,
+            firstname: firstNameReg,
+            lastname: lastNameReg,
+            phonenum: phoneNumberReg,
+            discordid: discordIDReg,
+            esportsgame: esportsGameReg 
         }).then((response) => {
             console.log(response)
         })
@@ -16,10 +22,11 @@ function Signup() {
     return (
       <div className=''>
           <h1>Registeration</h1>
-          <lable>Email</lable>
-          <input type='text' onChange={(e) => setEmailReg(e.target.value)}/>
-          <lable>Password</lable>
-          <input type='text'onChange={(e) => setPassReg(e.target.value)}/>
+          <input type='text' placeholder="First Name" onChange={(e) => setFirstNameReg(e.target.value)}/>
+          <input type='text' placeholder="Last Name" onChange={(e) => setLastNameReg(e.target.value)}/>
+          <input type='text' placeholder="Phone #" onChange={(e) => setPhoneNumberReg(e.target.value)}/>
+          <input type='text' placeholder="Discord ID" onChange={(e) => setDiscordIDReg(e.target.value)}/>
+          <input type='text' placeholder="Esports Game" onChange={(e) => setEsportsGameReg(e.target.value)}/>
           <button onClick={register}>Register</button>
       </div>
     )
